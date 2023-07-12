981ed771580d372d72c5d8903b57fcf8721f7169
Shawn Halayka <sjhalayka@gmail.com>
2023-07-11 18:11:36 -0600

    Add files via upload

--- img_train.py
+++ img_train.py
@@ -58,45 +58,6 @@ class Net(torch.nn.Module):
 	def forward(self, x):
 		return self.model(x)
 
-
-"""	
-def __init__(self, num_channels, num_output_components, all_train_files_len):
-		# call the parent constructor
-		super(Net, self).__init__()
-		self.conv1 = nn.Conv2d(num_channels, img_width, kernel_size=(3,3), stride=1, padding=1)
-		self.act1 = nn.ReLU()
-		self.drop1 = nn.Dropout(0.3)
- 
-		self.conv2 = nn.Conv2d(img_width, img_width, kernel_size=(3,3), stride=1, padding=1)
-		self.act2 = nn.ReLU()
-		self.pool2 = nn.MaxPool2d(kernel_size=(2, 2))
- 
-		self.flat = nn.Flatten()
- 
-		self.fc3 = nn.Linear(8*img_width*img_width, 512)
-		self.act3 = nn.ReLU()
-		self.drop3 = nn.Dropout(0.5)
- 
-		self.fc4 = nn.Linear(512, num_output_components)
-
-	def forward(self, x):
-		# input 3x32x32, output 32x32x32
-		x = self.act1(self.conv1(x))
-		x = self.drop1(x)
-		# input 32x32x32, output 32x32x32
-		x = self.act2(self.conv2(x))
-		# input 32x32x32, output 32x16x16
-		x = self.pool2(x)
-		# input 32x16x16, output 8192
-		x = self.flat(x)
-		# input 8192, output 512
-		x = self.act3(self.fc3(x))
-		x = self.drop3(x)
-		# input 512, output 10
-		x = self.fc4(x)
-		return x
-"""
-
 """
 	def __init__(self):
 		super(Net, self).__init__()
