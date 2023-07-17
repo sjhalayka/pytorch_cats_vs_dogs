26f1baedde3b26a0968e583ba7a8574d8d9fe890
Shawn Halayka <sjhalayka@gmail.com>
2023-07-16 19:57:05 -0600

    Add files via upload

--- img_train.py
+++ img_train.py
@@ -11,6 +11,7 @@ import torch.nn.functional as F
 import os.path
 from os import path
 
+import time
 
 
 
@@ -21,10 +22,10 @@ num_channels = 3
 #num_input_components = img_width*img_width*num_channels
 num_output_components = 2
 
-num_epochs = 1000
+num_epochs = 2
 learning_rate = 0.001
 
-max_train_files = 100000
+max_train_files = 100
 
 
 
@@ -32,7 +33,9 @@ max_train_files = 100000
 
 
 class Net(torch.nn.Module):
-	def __init__(self, num_channels, num_output_components, all_train_files_len):
+
+	def __init__(self, num_channels, num_output_components):
+
 		super().__init__()
 		self.model = torch.nn.Sequential(
 		    #Input = 3 x 32 x 32, Output = 32 x 32 x 32
@@ -62,21 +65,8 @@ class Net(torch.nn.Module):
 	def forward(self, x):
 		return self.model(x)
 
-"""
-	def __init__(self):
-		super(Net, self).__init__()
-		self.hidden1 = torch.nn.Linear(num_input_components, 8192)
-		self.hidden2 = torch.nn.Linear(8192, 1024) 
-		self.hidden3 = torch.nn.Linear(1024, 128)
-		self.predict = torch.nn.Linear(128, num_output_components)
 
-	def forward(self, x):
-		x = torch.tanh(self.hidden1(x))		
-		x = torch.tanh(self.hidden2(x))
-		x = torch.tanh(self.hidden3(x))
-		x = self.predict(x)    # linear output
-		return x
-"""
+
 
 
 
@@ -95,12 +85,58 @@ class image_type:
 
 
 
+def do_network(batch, ground_truth, num_channels, num_output_components, all_train_files, random_seed, num_epochs):
+
+	net = Net(num_channels, num_output_components)
+
+	random.seed(random_seed)
+
+	optimizer = torch.optim.Adam(net.parameters(), lr = learning_rate)
+	loss_func = torch.nn.MSELoss()
+
+	loss = 0;
+
+	for epoch in range(num_epochs):
+		
+		random.shuffle(all_train_files)
+
+		count = 0
+
+		for i in all_train_files:
+
+			batch[count] = i.float_img
+		
+			if i.img_type == 0: # cat
+
+				ground_truth[count][0] = 1
+				ground_truth[count][1] = 0
+			
+			elif i.img_type == 1: # dog
+				
+				ground_truth[count][0] = 0
+				ground_truth[count][1] = 1
+
+			count = count + 1
+	
+		x = Variable(torch.from_numpy(batch))
+		y = Variable(torch.from_numpy(ground_truth))
+
+		prediction = net(x)	 
+		loss = loss_func(prediction, y)
+
+		optimizer.zero_grad()	 # clear gradients for next train
+		loss.backward()		 # backpropagation, compute gradients
+		optimizer.step()		# apply gradients
+	
+	return net, loss
 
 
 
 
-if False: #path.exists('weights_' + str(num_input_components) + '_' + str(num_epochs) + '.pth'):
-	net.load_state_dict(torch.load('weights_' + str(num_input_components) + '_' + str(num_epochs) + '.pth'))
+
+
+if False: #path.exists('weights_' + str(img_width) + '_' + str(num_epochs) + '.pth'):
+	net.load_state_dict(torch.load('weights_' + str(img_width) + '_' + str(num_epochs) + '.pth'))
 	print("loaded file successfully")
 else:
 	print("training...")
@@ -170,53 +206,13 @@ else:
 
 
 
-	
-
-
-	net = Net(num_channels, num_output_components, len(all_train_files))
-	optimizer = torch.optim.Adam(net.parameters(), lr = learning_rate)
-	loss_func = torch.nn.MSELoss()
-
 	batch = np.zeros((len(all_train_files), num_channels, img_width, img_width), dtype=np.float32)
-	ground_truth = np.zeros((len(all_train_files), num_output_components), dtype=np.float32)
-	
-	for epoch in range(num_epochs):
-		
-		random.shuffle(all_train_files)
-
-		count = 0
-
-		for i in all_train_files:
-
-			batch[count] = i.float_img
-		
-			if i.img_type == 0: # cat
-
-				ground_truth[count][0] = 1
-				ground_truth[count][1] = 0
-			
-			elif i.img_type == 1: # dog
-				
-				ground_truth[count][0] = 0
-				ground_truth[count][1] = 1
-
-			count = count + 1
-	
-		x = Variable(torch.from_numpy(batch))
-		y = Variable(torch.from_numpy(ground_truth))
-
-		prediction = net(x)	 
-		loss = loss_func(prediction, y)
-
-		print(epoch, loss)
-
-		optimizer.zero_grad()	 # clear gradients for next train
-		loss.backward()		 # backpropagation, compute gradients
-		optimizer.step()		# apply gradients
+	ground_truth = np.zeros((len(all_train_files), num_output_components), dtype=np.float32)	
+	net, loss = do_network(batch, ground_truth, num_channels, num_output_components, all_train_files, round(time.time()), num_epochs)
 
 
 
-	#torch.save(net.state_dict(), 'weights_' + str(num_input_components) + '_' + str(num_epochs) + '.pth')
+#	torch.save(net.state_dict(), 'weights_' + str(img_width) + '_' + str(num_epochs) + '.pth')
 
 
 
